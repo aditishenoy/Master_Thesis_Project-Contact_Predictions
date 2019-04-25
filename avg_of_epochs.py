790a8db01e399b0401d559f5abf92bd4b70753fb
aditishenoy <aditi.shenoy@stud.ki.se>
2019-04-25 15:53:03 +0200

    calculated avg of precision, recall, errors and plotted different values of same model in same graph

---
+++ avg_of_epochs.py
@@ -0,0 +1,30 @@
+import glob
+import numpy as np 
+
+p_1 = glob.glob('/home/ashenoy/ashenoy/Thesis_unetplus_V.0.1/results_wednesday24/results_8_all_M26_R05_D01_E50_mae_trained')
+p_2 = glob.glob('/home/ashenoy/ashenoy/Thesis_unetplus_V.0.1/results_wednesday24/results_8_short_M26_R05_D01_E50_mae_trained')
+p_3 = glob.glob('/home/ashenoy/ashenoy/Thesis_unetplus_V.0.1/results_wednesday24/results_8_medium_M26_R05_D01_E50_mae_trained')
+p_4 = glob.glob('/home/ashenoy/ashenoy/Thesis_unetplus_V.0.1/results_wednesday24/results_8_long_M26_R05_D01_E50_mae_trained')
+
+avg_prec = []
+avg_rec = []
+avg_f1 = []
+avg_ab_err = []
+avg_rel_err = []
+
+def mean(lst): 
+    return sum(lst) / len(lst) 
+
+for f in (p_1):
+    d = np.loadtxt(f)
+    avg_prec = mean(d[:,1])
+    avg_rec = mean(d[:,2])
+    avg_f1 = mean(d[:,3])
+    avg_ab_err = mean(d[:,6])
+    avg_rel_err = mean(d[:,7])
+
+print (avg_prec)
+print (avg_rec)
+print (avg_f1)
+print (avg_ab_err)
+print (avg_rel_err)
